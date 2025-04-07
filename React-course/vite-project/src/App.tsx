b23b4b08d396c06e90cff2d7bf09495721a08527
durudaniel <danielchiemela16@gmail.com>
2025-04-07 15:18:26 +0100

    Starting with a react course

---
+++ React-course/vite-project/src/App.tsx
@@ -0,0 +1,11 @@
+import Message from "./components/ListGroup";
+
+function App() {
+  return (
+    <div>
+      <Message></Message>
+    </div>
+  );
+}
+
+export default App;
---
+++ React-course/vite-project/src/Message.tsx
@@ -0,0 +1,9 @@
+function Message() {
+  // JSX: Javascript XML
+
+  const name = "Daniel!";
+  if (name === "Daniel") return <h1>Hello {name}</h1>;
+  else return <h1>Hello World!</h1>;
+}
+
+export default Message;
