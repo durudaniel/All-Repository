35380c1e8f924aec1f7579cb869d4e2e1abd3eec
durudaniel <danielchiemela16@gmail.com>
2025-05-10 17:42:37 +0100

    Initialize to git

--- javascript_Math_and_Rounding/script.js
+++ javascript_Math_and_Rounding/script.js
@@ -126,3 +126,150 @@ const clockTimeInterval = setInterval(function () {
     )}: ${`${seconds}`.padStart(2, 0)}`
   );
 }, 3000)*/
+
+const data = [
+  {
+    id: 1,
+    title: "The Lord of The Rings",
+    publicationDate: "1954-07-29",
+    author: "J. R. R. Tolkien",
+    genres: [
+      "fantasy",
+      "high-fantasy",
+      "adventure",
+      "fiction",
+      "novels",
+      "literature",
+    ],
+    hasMovieAdaptation: true,
+    pages: 1216,
+    translations: {
+      spanish: "El senor de los anillos",
+      chinese: "##",
+      french: "Le seigneur des anneaux",
+    },
+    reviews: {
+      goodReads: {
+        ratings: 4.52,
+        ratingsCount: 630994,
+        reviewsCount: 13417,
+      },
+
+      libraryThing: {
+        rating: 4.53,
+        ratingsCount: 47166,
+        reviewsCount: 457,
+      },
+    },
+
+    id: 2,
+    title: "The Cyberiad",
+    publicationDate: "1965-0-01",
+    author: "Stanislaw lem",
+    genres: [
+      "science fiction",
+      "humor",
+      "speculative fiction",
+      "short stories",
+      "fantasy",
+    ],
+    hasMovieAdaptation: false,
+    pages: 295,
+    translations: {},
+    reviews: {
+      goodReads: {
+        ratings: 4.16,
+        ratingsCount: 11663,
+        reviewsCount: 812,
+      },
+
+      libraryThing: {
+        rating: 4.13,
+        ratingsCount: 2434,
+        reviewsCount: 8,
+      },
+    },
+
+    id: 3,
+    title: "Dune",
+    publicationDate: "1965-01-01",
+    author: "Frank Herbert",
+    genres: ["science fiction", "novel", "adventure"],
+    hasMovieAdaptation: true,
+    pages: 658,
+    translations: {
+      spanish: "",
+    },
+    reviews: {
+      goodReads: {
+        ratings: 4.25,
+        ratingsCount: 1142893,
+        reviewsCount: 49701,
+      },
+    },
+
+    id: 4,
+    title: "Harry Porter and the Philosopher's Stone",
+    publicationDate: "1997-06-26",
+    author: "J. K. Rowling",
+    genres: ["fantasy", "adventure"],
+    hasMovieAdaptation: true,
+    pages: 223,
+    translations: {
+      spanish: "Harry Potter y la piedra filosofal",
+      korean: "##",
+      bengali: "##",
+      portuguese: "Le seigneur des anneaux",
+    },
+    reviews: {
+      goodReads: {
+        ratings: 4.47,
+        ratingsCount: 8910059,
+        reviewsCount: 140025,
+      },
+
+      libraryThing: {
+        rating: 4.39,
+        ratingsCount: 120943,
+        reviewsCount: 1400,
+      },
+    },
+
+    id: 5,
+    title: "A Game of Throne",
+    publicationDate: "1996-08-01",
+    author: "George R. R. Martin",
+    genres: ["fantasy", "high-fantasy", "fiction", "novel", "fantasy fiction"],
+    hasMovieAdaptation: true,
+    pages: 1216,
+    translations: {
+      korean: "####",
+      polish: "Gra o tron",
+      french: "A Guerra dos Tremos",
+      spanish: "Juogo de tramos",
+    },
+    reviews: {
+      goodReads: {
+        ratings: 4.44,
+        ratingsCount: 2295233,
+        reviewsCount: 59058,
+      },
+
+      libraryThing: {
+        rating: 4.36,
+        ratingsCount: 38358,
+        reviewsCount: 1995,
+      },
+    },
+  },
+];
+
+function getBooks() {
+  return data;
+}
+
+function getBook() {
+  return data.find((d) => d.id === id);
+}
+
+c;
